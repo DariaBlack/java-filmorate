d2f0ec970fa9083d3256e5b1d5fe8b57b562a541
Daria <DariaProtos@yandex.ru>
2025-04-16 20:18:08 +0300

    Реализовать REST-контроллеры, без валидации

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,55 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/films")
 public class FilmController {
+    private final Map<Long, Film> films = new HashMap<>();
+
+    @PostMapping
+    public Film create(@RequestBody Film film) {
+
+
+        film.setId(getNextId());
+
+        films.put(film.getId(), film);
+        return film;
+    }
+
+    private long getNextId() {
+        long currentMaxId = films.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
+
+    @PutMapping
+    public Film update(@RequestBody Film newFilm) {
+
+        if (films.containsKey(newFilm.getId())) {
+            Film oldFilm = films.get(newFilm.getId());
+
+            oldFilm.setName(newFilm.getName());
+            oldFilm.setDescription(newFilm.getDescription());
+            oldFilm.setReleaseDate(newFilm.getReleaseDate());
+            oldFilm.setDuration(newFilm.getDuration());
+
+            return oldFilm;
+        }
+
+        throw new NegativeArraySizeException("Фильм с id = " + newFilm.getId() + " не найден");
+    }
+
+    @GetMapping
+    public Collection<Film> findAll() {
+        return films.values();
+    }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,4 +1,53 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+    private final Map<Long, User> users = new HashMap<>();
+
+    @PostMapping
+    public User create(@RequestBody User user) {
+
+        user.setId(getNextId());
+
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    private long getNextId() {
+        long currentMaxId = users.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
+
+    @PutMapping
+    public User update(@RequestBody User newUser) {
+        if (users.containsKey(newUser.getId())) {
+            User oldUser = users.get(newUser.getId());
+
+            oldUser.setEmail(newUser.getEmail());
+            oldUser.setLogin(newUser.getLogin());
+            oldUser.setName(newUser.getName());
+            oldUser.setBirthday(newUser.getBirthday());
+
+            return oldUser;
+        }
+
+        throw new NegativeArraySizeException("Пользователь с id = " + newUser.getId() + " не найден");
+    }
+
+    @GetMapping
+    public Collection<User> findAll() {
+        return users.values();
+    }
 }
