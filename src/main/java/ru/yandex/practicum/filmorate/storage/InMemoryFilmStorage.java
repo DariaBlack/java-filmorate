a4198883e5d2abb06a76262285a65db23f741cdf
Daria <DariaProtos@yandex.ru>
2025-04-26 16:59:40 +0300

    Вынести хранение данных о моделях в отдельные классы

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryFilmStorage.java
@@ -0,0 +1,47 @@
+package ru.yandex.practicum.filmorate.storage;
+
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class InMemoryFilmStorage implements FilmStorage {
+    private final Map<Long, Film> films = new HashMap<>();
+
+    @Override
+    public Film addFilm(Film film) {
+        film.setId(getNextId());
+        films.put(film.getId(), film);
+        return film;
+    }
+
+    @Override
+    public Film updateFilm(Film film) {
+        if (!films.containsKey(film.getId())) {
+            throw new RuntimeException("Фильм с id " + film.getId() + " не найден");
+        }
+        films.put(film.getId(), film);
+        return film;
+    }
+
+    @Override
+    public Film getFilm(Long id) {
+        return films.get(id);
+    }
+
+    @Override
+    public List<Film> getAllFilms() {
+        return new ArrayList<>(films.values());
+    }
+
+    private long getNextId() {
+        long currentMaxId = films.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryUserStorage.java
@@ -0,0 +1,48 @@
+package ru.yandex.practicum.filmorate.storage;
+
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class InMemoryUserStorage implements UserStorage {
+    private final Map<Long, User> users = new HashMap<>();
+    private long nextId = 1;
+
+    @Override
+    public User addUser(User user) {
+        user.setId(getNextId());
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    @Override
+    public User updateUser(User user) {
+        if (!users.containsKey(user.getId())) {
+            throw new RuntimeException("Пользователь с id " + user.getId() + " не найден");
+        }
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    @Override
+    public User getUser(Long id) {
+        return users.get(id);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return new ArrayList<>(users.values());
+    }
+
+    private long getNextId() {
+        long currentMaxId = users.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
+}
