1ff5b6f7fc35dc395c251e524cf216d7680785e0
Daria <DariaProtos@yandex.ru>
2025-04-26 18:54:37 +0300

    Добавить ErrorHandler для централизованной отработки ошибок

---
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/ErrorHandler.java
@@ -0,0 +1,30 @@
+package ru.yandex.practicum.filmorate.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler(ValidationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleValidationException(ValidationException e) {
+        return Map.of("error", e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String,String> handleNotFoundException(RuntimeException e) {
+        return Map.of("error", e.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String, String> handleInternalServerError(Exception e) {
+        return Map.of("error", "Внутренняя ошибка сервера: " + e.getMessage());
+    }
+}
