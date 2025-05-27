4e4b21bf8752281d86d35cb35fac345ca0368bba
Daria <DariaProtos@yandex.ru>
2025-05-27 13:23:27 +0300

    Сформировать структуру БД, реализовав ER-диаграмму

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,49 @@
+CREATE TABLE IF NOT EXISTS users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    email VARCHAR(255),
+    login VARCHAR(255),
+    birthday DATE
+);
+
+CREATE TABLE IF NOT EXISTS friends (
+    user_id INT,
+    friend_id INT,
+    status INT,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (friend_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE IF NOT EXISTS likes (
+    user_id INT,
+    film_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (film_id) REFERENCES films(film_id)
+);
+
+CREATE TABLE IF NOT EXISTS films (
+    film_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    description VARCHAR(255),
+    releaseDate DATE,
+    duration INT,
+    rating_id INT,
+    FOREIGN KEY (rating_id) REFERENCES rating_mpaa(rating_id)
+);
+
+CREATE TABLE IF NOT EXISTS rating_mpaa (
+    rating_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS film_genre (
+    film_id INT,
+    genre_id INT,
+    FOREIGN KEY (film_id) REFERENCES films(film_id),
+    FOREIGN KEY (genre_id) REFERENCES genre(genre_id)
+);
+
+CREATE TABLE IF NOT EXISTS genre (
+    genre_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255)
+);
