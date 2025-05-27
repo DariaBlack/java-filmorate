00ed16a294d4a34ac5ec668093526ef6ead527f0
Daria <DariaProtos@yandex.ru>
2025-05-27 18:35:24 +0300

    Создать класс для интеграционного тестирования

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,13 +1,30 @@
 package ru.yandex.practicum.filmorate;
 
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.core.JdbcTemplate;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.storage.UserDbStorage;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JdbcTest
+@AutoConfigureTestDatabase
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+@Import({UserDbStorage.class})
 class FilmorateApplicationTests {
+    private final UserDbStorage userStorage;
+    private final JdbcTemplate jdbcTemplate;
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    public void testFindUserById() {
+        User user = userStorage.getUser(1L);
 
+        assertThat(user).isNotNull();
+        assertThat(user).hasFieldOrPropertyWithValue("id", 1L);
+    }
 }
