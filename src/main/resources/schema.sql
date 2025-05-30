f801d9dac0888d4c82bcbe7e4e1d4f25f851ca42
Daria <DariaProtos@yandex.ru>
2025-05-30 22:16:30 +0300

    Задать логичные значения для текстовых полей в БД. Удалить каталог resources из тестовой ветки. Убрать тесты из проекта.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,50 +1,46 @@
-DELETE FROM likes;
-DELETE FROM film_genre;
-DELETE FROM friends;
-
-DELETE FROM films;
-DELETE FROM users;
-DELETE FROM rating_mpaa;
-DELETE FROM genre;
-
-ALTER TABLE films ALTER COLUMN film_id RESTART WITH 1;
-ALTER TABLE users ALTER COLUMN user_id RESTART WITH 1;
+DROP TABLE IF EXISTS likes;
+DROP TABLE IF EXISTS film_genre;
+DROP TABLE IF EXISTS friends;
+DROP TABLE IF EXISTS films;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS rating_mpaa;
+DROP TABLE IF EXISTS genre;
 
 CREATE TABLE IF NOT EXISTS users (
-    user_id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    email VARCHAR(255) NOT NULL,
-    login VARCHAR(255) NOT NULL,
-    name VARCHAR(255),
-    birthday DATE
+                                     user_id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                                     email VARCHAR(100) NOT NULL,
+                                     login VARCHAR(50) NOT NULL,
+                                     name VARCHAR(100),
+                                     birthday DATE
 );
 
 CREATE TABLE IF NOT EXISTS rating_mpaa (
-    rating_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL UNIQUE
+                                           rating_id INT AUTO_INCREMENT PRIMARY KEY,
+                                           name VARCHAR(5) NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS films (
-    film_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    description VARCHAR(255),
-    release_date DATE,
-    duration INT,
-    rating_id INT,
-    FOREIGN KEY (rating_id) REFERENCES rating_mpaa(rating_id)
+                                     film_id INT AUTO_INCREMENT PRIMARY KEY,
+                                     name VARCHAR(200) NOT NULL,
+                                     description VARCHAR(1000),
+                                     release_date DATE,
+                                     duration INT,
+                                     rating_id INT,
+                                     FOREIGN KEY (rating_id) REFERENCES rating_mpaa(rating_id)
 );
 
 CREATE TABLE IF NOT EXISTS genre (
-    genre_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL UNIQUE
+                                     genre_id INT AUTO_INCREMENT PRIMARY KEY,
+                                     name VARCHAR(50) NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS friends (
-    user_id INT,
-    friend_id INT,
-    status INT,
-    PRIMARY KEY (user_id, friend_id),
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (friend_id) REFERENCES users(user_id)
+                                       user_id INT,
+                                       friend_id INT,
+                                       status INT,
+                                       PRIMARY KEY (user_id, friend_id),
+                                       FOREIGN KEY (user_id) REFERENCES users(user_id),
+                                       FOREIGN KEY (friend_id) REFERENCES users(user_id)
 );
 
 CREATE TABLE IF NOT EXISTS likes (
@@ -56,9 +52,9 @@ CREATE TABLE IF NOT EXISTS likes (
 );
 
 CREATE TABLE IF NOT EXISTS film_genre (
-    film_id INT,
-    genre_id INT,
-    PRIMARY KEY (film_id, genre_id),
-    FOREIGN KEY (film_id) REFERENCES films(film_id),
-    FOREIGN KEY (genre_id) REFERENCES genre(genre_id)
+                                          film_id INT,
+                                          genre_id INT,
+                                          PRIMARY KEY (film_id, genre_id),
+                                          FOREIGN KEY (film_id) REFERENCES films(film_id),
+                                          FOREIGN KEY (genre_id) REFERENCES genre(genre_id)
 );
